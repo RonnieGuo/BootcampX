9291da6cbc625a56ee0f1ea1e3fcae3ba7acbab9
RonnieGuo <guorongfly@gmail.com>
2023-08-07 14:54:57 -0600

    use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -24,6 +24,7 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
 LIMIT 5;
 `)
 .then(res => {
@@ -32,17 +33,24 @@ LIMIT 5;
   })
 });
 
-//Allow a user to specify a cohort name "node students.js FEB 2"
+// Get cohort name and limit from command-line arguments
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
 
-pool.query(`
+// Create the SQL query with placeholders for cohort name and limit
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+// Execute the query with the specified cohort name and limit
+pool.query(queryString, [`%${cohortName}%`, limit])
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,39 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
+
+// Get cohort name from command-line argument
+const cohortName = process.argv[2];
+
+// Create the SQL query with placeholder for cohort name
+const queryString = `
+SELECT DISTINCT teachers.name
 FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = $1;
+`;
+
+// Execute the query with the specified cohort name
+pool.query(queryString, [cohortName])
+  .then(res => {
+    res.rows.forEach(teacher => {
+      console.log(`${cohortName}: ${teacher.name}`);
+    });
   })
-});
+  .catch(err => console.error('query error', err.stack));
